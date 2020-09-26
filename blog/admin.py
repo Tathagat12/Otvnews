f5337b5e9b45d4e2a75182fa9fc90f2b87d708c2
unknown <tathagat.dalai@gmail.com>
2020-09-26 17:22:24 +0530

    Intial commit

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post,Author,Categorie,Subscribe,Contact,Comment,Subcomment
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Categorie)
+admin.site.register(Subscribe)
+admin.site.register(Contact)
+admin.site.register(Comment)
+admin.site.register(Subcomment)
